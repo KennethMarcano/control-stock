0194673ed73b5f2812d83a6b05914559356d1d64
Kenneth <kennethmarcano96@outlook.com>
2024-08-11 00:51:47 -0300

    added message: need login

--- src/routes/MyRoutes.js
+++ src/routes/MyRoutes.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 
 export default function MyRoute({ component: Component, isClosed = false, ...rest }) {
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
-    if(isClosed && !isLoggedIn) {
-        return(
-            <Redirect
-                to={{ pathname: '/', state: { prevPath: rest.location.pathname } }}
-            />
+    if (isClosed && !isLoggedIn) {
+        return (
+            <>
+                {toast.warn('Precisa fazer login!')}
+                <Redirect
+                    to={{ pathname: '/', state: { prevPath: rest.location.pathname } }}
+                />
+            </>
+
         )
     }
 
-    return <Route {...rest} component={Component}/>;
+    return <Route {...rest} component={Component} />;
 };
 
 MyRoute.prototype = {
